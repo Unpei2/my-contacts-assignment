78e006c985f6dc9875332035b25c35b731ab849f
Unpei2 <r.ho5@share.epsb.ca>
2022-10-05 14:48:10 -0600

    i have cancer

--- main.js
+++ main.js
@@ -4,8 +4,9 @@
 let goBtnEl = document.getElementById('go-btn');
 let menuEl = document.getElementById('menu');
 let outputEl = document.getElementById('output');
+let contacts = loadContacts()
 
-let contacts = []
+
 // Go Btn - Menu Listener
 goBtnEl.addEventListener('click', goBtnHandler);
 
@@ -25,27 +26,37 @@ function goBtnHandler() {
     displayByCountry();
   }
 }
-let contactstring = JSON.stringify(contacts)
 // MENU FUNCTIONS
 function displayContacts() {
-  console.log('Display Contacts');
-  outputEl.innerHTML = contactstring
   
-}
+  for (i = 0;i<contacts.length;i ++){
+    let output = " "
+    output += getContactHTMLStr(i,contacts[i])
+  }
+  outputEl.innerHTML = output
 
+}
+function getContactHTMLStr(i, contact){
+  return `
+    <div class="${contact.name}">
+      <h3>${i}: ${contact.name}</h3>
+      <p>${contact.email}</p>
+      <p>${contact.number}(${contact.country})<p>
+    </div>
+  `;
+}
 function addContact() {
   let contname = prompt("Contact Name?")
   let contemail = prompt("Contact Email?")
   let contnumber = prompt("Contact Number?")
   let contcountry = prompt("Contact Country?")
   contacts.push(information(contname, contemail, contnumber, contcountry));
-  let contactstring = JSON.stringify(contacts)
-  localStorage.setItem(contname, contactstring)   
-  displayContacts() 
+  savecont()
 }
 
 function removeContact() {
-  console.log('Remove Contact');
+  let removecont = +prompt("Which contact do you want to remove?")
+  contacts.splice(removecont, 1)
 }
 
 function displayByName() {
@@ -64,4 +75,13 @@ function information(name,email,number,country) {
     number, 
     country
   }
+}
+function loadContacts(){
+  let contactsStr = localStorage.getItem("Contacts");
+  return JSON.parse(contactsStr) ?? []
+}
+function savecont(){
+  let contactstring = JSON.stringify(contacts)
+  localStorage.setItem("Contacts", contactstring)   
+  displayContacts() 
 }
